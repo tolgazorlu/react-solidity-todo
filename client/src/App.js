b674b5a2ada1512cfebe00add6b91f1917065375
tolgazorlu <tolgazorlu17@gmail.com>
2023-03-01 19:12:35 +0300

    ui added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 
+import Header from './components/Header'
 import Todo from './components/Todo'
 
 export default function App() {
 
   return(
-    <div>
+    <div className="bg-light w-full h-screen overflow-hidden z-0">
+      <div>
+        <img className="absolute bottom-1 left-10" alt="blur-cyan" src={require('./assets/blur-cyan.png')} />
+        <img className="absolute bottom-1 right-10" alt="blur-indigo" src={require('./assets/blur-indigo.png')} />
+      </div>
+      <Header />
       <Todo />
     </div>
   )
 
 }
+
+/**
+ * <h1 className="bg-dark w-full h-screen overflow-hidden z-0">
+      <div className="">
+        <img className="absolute bottom-1 left-10" alt="blur-cyan" src={require('./assets/blur-cyan.png')} />
+        <img className="absolute bottom-1 right-10" alt="blur-indigo" src={require('./assets/blur-indigo.png')} />
+      </div>
+      <Header />
+      <Main />
+      <Footer />
+    </h1>
+ */
